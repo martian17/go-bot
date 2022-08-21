8c3f21874473a8471f844a79f4124e58fe7bcd85
martian17 <yakumodanshi@gmail.com>
2022-08-21 12:27:57 +0900

    organize routine functions

--- main.go
+++ main.go
@@ -37,14 +37,38 @@ func loadCredentials() {
 }
 
 
-func sendMessage(channel string, msg string){
+
+//main functions
+
+//when payload == nil
+//for optimization but not sure if im gonna use it
+func makeRequestNil (method string, url string) (*http.Response, error) {
+    url = "https://discord.com/api/v10"+url
     client := &http.Client{}
-    //https://discord.com/channels/1010708479142547547/1010708479742320763
-    req, err := http.NewRequest("POST", "https://discord.com/api/v10/channels/"+channel+"/messages", 
-        strings.NewReader("{\"content\":\""+msg+"\"}"))
-	if err != nil {
-		log.Println(": ", err)
-        return
+    req, err := http.NewRequest(method, url, nil)
+    if err != nil {
+        log.Println("could not create request: ")
+        return nil, err
+	}
+    
+	req.Header.Set("Authorization", "Bot " + token)
+    //req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+    if err != nil {
+		log.Println("request do failed: ")
+        return nil, err
+	}
+    return resp, nil
+}
+
+func makeRequest (method string, url string, payload string) (*http.Response, error) {
+    url = "https://discord.com/api/v10"+url
+    client := &http.Client{}
+    req, err := http.NewRequest(method, url, strings.NewReader(payload))
+    if err != nil {
+        log.Println("could not create request: ")
+        return nil, err
 	}
     
 	req.Header.Set("Authorization", "Bot " + token)
@@ -52,41 +76,57 @@ func sendMessage(channel string, msg string){
 
 	resp, err := client.Do(req)
     if err != nil {
-		log.Println(": ",err)
-        return
+		log.Println("request do failed: ")
+        return nil, err
 	}
-    
-    defer resp.Body.Close()
+    return resp, nil
 }
 
-func printGetRequest(url string){
-    client := &http.Client{}
-    //https://discord.com/channels/1010708479142547547/1010708479742320763
-    req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(": ", err)
-	}
-    
-	req.Header.Set("Authorization", "Bot " + token)
-    //req.Header.Set("Content-Type", "application/json")
+func sendMessage(channel string, msg string) error {
+    _,err := makeRequest("POST","/channels/"+channel+"/messages",msg)
+    return err
+}
 
-	resp, err := client.Do(req)
+func printRequest(method string, url string, payload string) {
+	resp, err := makeRequest(method,url,payload)
+    if err != nil {
+		log.Println(err)
+        return
+	}
     
     defer resp.Body.Close()
 
 	b, err := io.ReadAll(resp.Body)
     if err != nil {
-		log.Fatal(": ",err)
+		log.Println("request body read failed: ",err)
+        return
 	}
 
 	fmt.Println(string(b))
 }
 
+
 func main(){
     loadCredentials()
-    sendMessage("1010708479742320763","hi there")
-    printGetRequest("https://discord.com/api/v10/channels/1010708479742320763")
-    printGetRequest("https://discord.com/api/v10/channels/1010708479742320763/messages")
+    
+    //https://discord.com/channels/1010708479142547547/1010708479742320763
+    
+    //example codes
+    //post message
+    //sendMessage("1010708479742320763","hi there")
+    
+    //get channel state
+    //printRequest("GET","/channels/1010708479742320763","")
+    
+    //get channel message history
+    //printRequest("GET","/channels/1010708479742320763/messages","")
+    
+    //get list of registered slash commands
+    printRequest("GET","/applications/"+appid+"/commands","")
+    
+    //delete command
+    //printRequest("DELETE","/applications/"+appid+"/commands/1010341986890420224","")
+    
 }
 
 
